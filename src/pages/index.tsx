d7b7957b5493c5d4e82f5e7997600aa11930a45f
Rohid <rohidul209@gmail.com>
2023-08-27 22:53:50 +0600

    bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,9 @@ import MarketingHeader from "@/components/MarketingHeader";
 import { trpc } from "@/utils/trpc";
 
 const Page = () => {
-  const user = trpc.user.useQuery();
   return (
     <>
       <MarketingHeader />
-      <pre>{JSON.stringify({ user: user.data?.userId }, null, 2)}</pre>
     </>
   );
 };
