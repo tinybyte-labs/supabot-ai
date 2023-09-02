92283b3ddcbc3f58a27865df26c856e261c3b44a
Rohid <rohidul209@gmail.com>
2023-09-02 22:35:11 +0600

    try catch on clerk webhook

--- src/pages/api/webhooks/clerk.ts
+++ src/pages/api/webhooks/clerk.ts
@@ -18,50 +18,53 @@ export default async function handlers(
     console.log("Clerk Event:", event.data.type);
     console.log(event.data);
 
-    switch (event.data.type) {
-      case "user.created":
-        await caller.clerk.userCreated(event.data.data);
-        break;
-      case "user.updated":
-        await caller.clerk.userUpdated(event.data.data);
-        break;
-      case "user.deleted":
-        await caller.clerk.userDeleted(event.data.data);
-        break;
-      case "organization.created":
-        await caller.clerk.organizationCreated(event.data.data);
-        break;
-      case "organization.updated":
-        await caller.clerk.organizationUpdated(event.data.data);
-        break;
-      case "organization.deleted":
-        await caller.clerk.organizationDeleted(event.data.data);
-        break;
-      case "organizationMembership.created":
-        await caller.clerk.organizationMembershipCreated(event.data.data);
-        break;
-      case "organizationMembership.updated":
-        await caller.clerk.organizationMembershipUpdated(event.data.data);
-        break;
-      case "organizationMembership.deleted":
-        await caller.clerk.organizationMembershipDeleted(event.data.data);
-        break;
-      case "session.created":
-        await caller.clerk.sessionCreated(event.data.data);
-        break;
-      case "session.ended":
-        await caller.clerk.sessionEnded(event.data.data);
-        break;
-      case "session.removed":
-        await caller.clerk.sessionRemoved(event.data.data);
-        break;
-      case "session.revoked":
-        await caller.clerk.sessionRevoked(event.data.data);
-        break;
-      default:
-        break;
+    try {
+      switch (event.data.type) {
+        case "user.created":
+          await caller.clerk.userCreated(event.data.data);
+          break;
+        case "user.updated":
+          await caller.clerk.userUpdated(event.data.data);
+          break;
+        case "user.deleted":
+          await caller.clerk.userDeleted(event.data.data);
+          break;
+        case "organization.created":
+          await caller.clerk.organizationCreated(event.data.data);
+          break;
+        case "organization.updated":
+          await caller.clerk.organizationUpdated(event.data.data);
+          break;
+        case "organization.deleted":
+          await caller.clerk.organizationDeleted(event.data.data);
+          break;
+        case "organizationMembership.created":
+          await caller.clerk.organizationMembershipCreated(event.data.data);
+          break;
+        case "organizationMembership.updated":
+          await caller.clerk.organizationMembershipUpdated(event.data.data);
+          break;
+        case "organizationMembership.deleted":
+          await caller.clerk.organizationMembershipDeleted(event.data.data);
+          break;
+        case "session.created":
+          await caller.clerk.sessionCreated(event.data.data);
+          break;
+        case "session.ended":
+          await caller.clerk.sessionEnded(event.data.data);
+          break;
+        case "session.removed":
+          await caller.clerk.sessionRemoved(event.data.data);
+          break;
+        case "session.revoked":
+          await caller.clerk.sessionRevoked(event.data.data);
+          break;
+        default:
+          break;
+      }
+      return res.json({ success: true });
+    } catch (error) {
+      return res.status(500);
     }
-
-    return res.json({ success: true });
   }
 }
