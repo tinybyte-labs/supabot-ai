70c59fc46fbf04c18bd7241aa4328fb2d592b2bc
Rohid <rohidul209@gmail.com>
2023-09-10 10:59:07 +0600

    build fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["images.unsplash.com"],
   },
