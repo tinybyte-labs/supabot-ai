855d2917775d027ad3c635e0fc10ece1938c491b
Rohid <rohidul209@gmail.com>
2023-09-19 08:46:55 +0600

    Theme for Auth

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,14 +1,15 @@
 import AuthHeader from "@/components/AuthHeader";
 import DevWarningBar from "@/components/DevWarningBar";
+import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
+    <ThemeProvider enableSystem attribute="class">
       <DevWarningBar />
       <AuthHeader />
       {children}
-    </>
+    </ThemeProvider>
   );
 };
 
