9119aa94dca823eefbd992167e9d759aea899173
Rohid <rohidul209@gmail.com>
2023-09-21 19:18:59 +0600

    small fix

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -1,6 +1,7 @@
 export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "";
 
 export const pageView = (page_path: string) => {
+  if (!MEASUREMENT_ID) return;
   gtag("config", MEASUREMENT_ID, {
     page_path,
   });
