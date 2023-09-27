6901ef361866afc7373c1f7df59084ea7128ca56
Rohid <rohidul209@gmail.com>
2023-09-27 14:53:47 +0600

    web: community links added

--- apps/web/src/components/pages/home/Hero.tsx
+++ apps/web/src/components/pages/home/Hero.tsx
@@ -9,12 +9,12 @@ const Hero = () => {
     <section id="hero" className="py-24">
       <div className="container">
         <div className="mx-auto max-w-screen-sm md:max-w-screen-lg">
-          <div className="bg-gradient-to-br from-accent-foreground to-accent-foreground/70 bg-clip-text py-1">
+          <div className="from-accent-foreground to-accent-foreground/70 bg-gradient-to-br bg-clip-text py-1">
             <h1 className="text-center text-4xl font-bold tracking-tight text-transparent md:text-5xl lg:text-7xl">
               {HERO_TITLE}
             </h1>
           </div>
-          <p className="mt-6 text-center font-medium text-muted-foreground md:text-lg lg:text-xl">
+          <p className="text-muted-foreground mt-6 text-center font-medium md:text-lg lg:text-xl">
             {HERO_SUBTITLE}
           </p>
 
@@ -35,10 +35,7 @@ const Hero = () => {
               size="lg"
               variant="outline"
             >
-              <Link
-                href="https://github.com/iam-rohid/supabot-ai"
-                target="_blank"
-              >
+              <Link href="/github" target="_blank">
                 <Star size={24} className="-ml-2 mr-4" />
                 Star on Github
               </Link>
@@ -63,7 +60,7 @@ const Hero = () => {
             className="w-full rounded-lg border dark:hidden md:rounded-2xl"
             quality={60}
           />
-          <div className="absolute inset-0 -z-10 bg-foreground/5 blur-3xl"></div>
+          <div className="bg-foreground/5 absolute inset-0 -z-10 blur-3xl"></div>
         </div>
       </div>
     </section>
