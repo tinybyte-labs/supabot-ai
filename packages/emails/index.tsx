a6be0f77620b255d5c943109580210d8d676961b
Rohid <rohidul209@gmail.com>
2023-09-27 23:00:14 +0600

    build fix

--- packages/emails/index.tsx
+++ packages/emails/index.tsx
@@ -1,13 +1,13 @@
 import { Resend } from "resend";
 import WelcomeEmail from "./emails/welcome";
-import * as React from "react";
+import type { ReactElement } from "react";
 
 export { WelcomeEmail };
 
 export const resend = new Resend(process.env.RESEND_API_KEY);
 
 export interface Emails {
-  react: React.ReactElement;
+  react: ReactElement;
   subject: "Welcome to SupaBot AI 👋";
   to: string[];
   from: "Rohid <rohid@supabotai.com>";
