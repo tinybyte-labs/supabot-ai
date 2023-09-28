f60d88b2d548a1527711f5f29a7b2f3ee24b6feb
Rohid <rohidul209@gmail.com>
2023-09-28 12:52:03 +0600

    refactor: move plans to a separate package

---
+++ packages/plans/src/types.ts
@@ -0,0 +1,24 @@
+export type PlanInterval = "monthly" | "yearly";
+
+export type Plan = {
+  id: string;
+  name: string;
+  limits: {
+    chatbots: number | "unlimited";
+    messagesPerMonth: number | "unlimited";
+    links: number | "unlimited";
+    documents: number | "unlimited";
+    teamMembers: number | "unlimited";
+    advancedCustomization: boolean;
+    removeWatermark: boolean;
+    apiAccess: boolean;
+    prioritySupport: boolean;
+  };
+  price: Record<
+    PlanInterval,
+    {
+      amount: number;
+      priceId: string;
+    }
+  >;
+};
