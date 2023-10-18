cb74807cc6a48cf057503ede2249f45a5ef7de48
Rohid <rohidul209@gmail.com>
2023-10-18 10:47:06 +0600

    fix: gtag removed

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,14 +3,12 @@ import "@/styles/globals.css";
 import { AppPropsWithLayout } from "@/types/next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAtom } from "jotai";
 import { sidebarOpenAtom } from "@/atoms/sidebarOpen";
 import Head from "next/head";
 import { APP_NAME } from "@/utils/constants";
-import Script from "next/script";
-import * as gtag from "@/utils/gtag";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Elements } from "@stripe/react-stripe-js";
 import { stripePromise } from "@/lib/stripe";
@@ -28,30 +26,21 @@ function App({
   const router = useRouter();
   const [sidebarOpen, setSidebarOpen] = useAtom(sidebarOpenAtom);
 
-  const closeSidebar = useCallback(() => {
-    setSidebarOpen(false);
-  }, [setSidebarOpen]);
-
-  const onRouteChangeComplete = (e: any) => {
-    gtag.pageView(e);
-  };
-
-  useEffect(() => {
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
-    };
-  }, [router.events]);
-
   useEffect(() => {
     if (!sidebarOpen) return;
+
+    const closeSidebar = () => {
+      setSidebarOpen(false);
+    };
+
     router.events.on("routeChangeStart", closeSidebar);
     window.addEventListener("resize", closeSidebar);
+
     return () => {
       router.events.off("routeChangeStart", closeSidebar);
       window.removeEventListener("resize", closeSidebar);
     };
-  }, [closeSidebar, router.events, sidebarOpen]);
+  }, [router.events, setSidebarOpen, sidebarOpen]);
 
   return (
     <PlausibleProvider domain="supabotai.com">
@@ -62,20 +51,6 @@ function App({
               <Head>
                 <title>{APP_NAME}</title>
               </Head>
-              <Script
-                strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.MEASUREMENT_ID}`}
-              />
-              <Script id="google-analytics" strategy="lazyOnload">
-                {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${gtag.MEASUREMENT_ID}', {
-                  page_path: window.location.pathname,
-                  });
-                `}
-              </Script>
               {getLayout(<Component {...pageProps} />)}
             </div>
           </TooltipProvider>
--- apps/web/src/utils/gtag.ts
+++
@@ -1,8 +0,0 @@
-export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "";
-
-export const pageView = (page_path: string) => {
-  if (!MEASUREMENT_ID) return;
-  gtag("config", MEASUREMENT_ID, {
-    page_path,
-  });
-};
