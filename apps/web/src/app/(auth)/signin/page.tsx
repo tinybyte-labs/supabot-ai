057c50242a3733ffe0db456cea24441c5b488dd8
Rohid <rohidul209@gmail.com>
2023-12-02 23:01:04 +0600

    feat(web): email signin

--- apps/web/src/app/(auth)/signin/page.tsx
+++ apps/web/src/app/(auth)/signin/page.tsx
@@ -11,6 +11,7 @@ import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@acme/auth";
+import EmailSignInForm from "./EmailSignInForm";
 
 export const metadata: Metadata = {
   title: `Sign In - ${APP_NAME}`,
@@ -31,6 +32,12 @@ export default async function SignInPage() {
           <CardDescription>to continue to {APP_NAME}</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-4">
+          <EmailSignInForm />
+          <div className="my-4 flex items-center gap-4">
+            <div className="bg-border h-px flex-1"></div>
+            <p className="text-muted-foreground text-sm">OR</p>
+            <div className="bg-border h-px flex-1"></div>
+          </div>
           <SignInButtons />
         </CardContent>
       </Card>
